4bd4e6b46b9759c921e396432571f7e93fb8b11c
Sarah <sarahlchavez@gmail.com>
2021-11-24 03:31:26 -0800

    commit tree 0b1362ec1f8feb51c2f8c8afc2943395ed947062

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,48 @@
-export function manageFriends(state, action){
+export function manageFriends(state = { friends: []}, action){
+    switch (action.type) {
+        
+        case "ADD_FRIEND":
+            return (
+            {...state,
+                friends: [
+                    ...state.friends,
+                    action.friend
+                ]
+            } 
+        )
+    
+    case "REMOVE_FRIEND":
+        const removalIndex = state.friends.findIndex(friend => friend.id === action.id)
+        return (
+            {...state,
+                friends: [
+                    ...state.friends.slice(0, removalIndex),
+                    ...state.friends.slice(removalIndex +1)
+                ]
+            
+        }
+        )
+        default:
+            return state;
+    }
 }
+    
+    // action = {
+    //     type: "ADD_FRIEND",
+    //     friend: {
+    //         name: "",
+    //         hometown: "",
+    //         id: "", 
+    //     }
+        // return a new state with this friend object 
+        // added to the friends array
+//         return { friends: state.friends }
+//     }
+
+//     action = {
+//         type: "REMOVE_FRIEND",
+//         id: "",
+//     };
+// }
+
+
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,16 @@
-export function managePresents(state, action){
+export function managePresents(state = { numberOfPresents: 0}, action){
+    switch (action.type) {
+        case "INCREASE":
+            return { numberOfPresents: state.numberOfPresents + 1 };
+        default:
+            return state;
+    }
+ 
 }
+
+// write a function called managePresents() 
+// that takes in a previous state and an 
+// action as its argument.
+
+// set a default value for the state argument
+// an object with a key numberOfPresents, assigned to 0
